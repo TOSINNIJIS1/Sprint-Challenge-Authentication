0b38d8ed73bc5006d4cbd065eb7d28c19153a683
VINCENT ADENIJI <adenijitosin112@gmail.com>
2020-05-03 23:52:04 -0400

    MVP

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,28 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secret.js');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    console.log(token) 
+
+    if(token) {
+      jwt.verify(token, secrets.jwt_secret, (err, decodedToken) => {
+        if (err) {
+          res.status(401).json({message: "bad auth"});
+        } else {
+          req.decodedToken = decodedToken;
+          next()
+        }
+      }) 
+    } else {
+      throw new Error("bad authorization")
+    }
+  } catch(err) {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
